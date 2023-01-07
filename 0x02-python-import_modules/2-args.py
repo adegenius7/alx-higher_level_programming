575e34cd91820f4076504efc684da0b0c40312f3
adegenius7 <adegenius007@gmail.com>
2023-01-07 17:19:06 +0000

    import sys argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
         print("0 arguments.")
     elif length == 1:
         print("1 argument:")
-        print("1: {}".format(arg[1]), end='\n')
+        print("1: {}".format(arg[1]))
     else:
         print("{} arguments:".format(length))
     for i in range(length):
-        print("{}: {}".format(i + 1, arg[i + 1]), end='\n')
+        print("{}: {}".format(i + 1, arg[i + 1]))
