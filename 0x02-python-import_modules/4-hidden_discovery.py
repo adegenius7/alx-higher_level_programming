526fb8a2b46d997014a01ee40bd80e3ec28840e7
adegenius7 <adegenius007@gmail.com>
2023-01-07 19:12:37 +0000

    using dir() to extract an object file

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import hidden_4
+    '''printing hidden names'''
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != '__':
+            print(name)
