95445957a5863d11a1c8662f2fe9544debd222c6
adegenius7 <adegenius007@gmail.com>
2023-01-07 17:06:01 +0000

    import sys argument

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    '''counting the number of args '''
+    import sys
+    arg = sys.argv
+    length = len(arg) - 1
+    if length == 0:
+        print("0 arguments.")
+    elif length == 1:
+        print("1 argument:")
+        print("1: {}".format(arg[1]), end='\n')
+    else:
+        print("{} arguments:".format(length))
+    for i in range(length - 1):
+        print("{}: {}".format(i, arg[i + 1]), end='\n')
