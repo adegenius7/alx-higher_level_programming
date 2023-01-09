afb0116890f6a97901dd11ca6c4a9c4ec03ec6c9
adegenius7 <adegenius007@gmail.com>
2023-01-09 18:54:53 +0000

    a function that returns the max value using sort

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,9 +2,8 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     else:
-        for i in my_list:
-            if my_list[i] < my_list[0]:
-                my_list[0] = my_list[i]
-            else:
+        maximum = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > maximum:
                 maximum = my_list[i]
-            return maximum
+        return maximum
