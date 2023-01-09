7591aaa8bb97d0cd6755bdfe841e9e7226544943
adegenius7 <adegenius007@gmail.com>
2023-01-09 17:36:33 +0000

    a function that reveals lenght of a tuple

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        sentence[0] is None
+    else:
+        a = len(sentence)
+        b = sentence[0]
+        return a, b
